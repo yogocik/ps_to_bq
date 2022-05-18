6747b525222f8b03adadec1ab58c13e6192e73b5
Yogic <yogicrhama@gmail.com>
2022-05-18 15:43:04 +0700

    Edited route processing script

--- break.py
+++ break.py
@@ -3,6 +3,7 @@ import json
 from multiprocessing import dummy
 from google.cloud import bigquery
 import pytz
+import typing
 
 PROJECT_ID = "trial-project-star-capital"
 DATASET_ID = "CDC"
@@ -46,6 +47,7 @@ dummy_data_2 = {
   "before": {
     "id": 1,
     "name": "Damian",
+    "code": "XAM",
     "created_at": 1652257309034061,
     "updated_at": 1652257309034061
   },
@@ -59,7 +61,7 @@ dummy_data_2 = {
     "db": "data_cdc_testdb",
     "sequence": "[\"9658244328\",\"9658244528\"]",
     "schema": "public",
-    "table": "testings",
+    "table": "random",
     "txId": 2588573,
     "lsn": 9658244528,
     "xmin": ""
@@ -73,72 +75,147 @@ payload_data = {
     "payload": dummy_data_2
 }
 
-table_prefix = "raw_cdc"
+# COPY FROM HERE INTO CLOUD FUNCTION
 
-def routing_table(prefix: str =table_prefix) -> dict:
-    table_list = [("testing", "testing"), ("random", "random")]
-    route = {elem[0]: "_".join([prefix, elem[1]]) for elem in table_list}
+table_prefix = "cdc_raw"                                    # BigQuery table's prefix for raw dataset
+table_list = [("testing", "testing"), ("random", "random")] # [(source_table_name, bq_table_name)] source-dataset table pairs for routing
+table_key_index = {"testing": ["id"],
+                   "random": ["id", "code"]}                # List of key index in certain sources include primary or composite key
+data_change_attr = ["after","before"]                       # Message body attribute where data change log was written
+time_key_list = ["ts_ms","create", "update"]                # List of substring of key attribute for time data in source payload
+tz = pytz.timezone('Asia/Jakarta')                          # Predefined timezone for time conversion
+
+def routing_table(router: typing.List[typing.Tuple[str,str]] = table_list, 
+                  prefix: str =table_prefix) -> dict:
+    """Route source data into specific big query table
+
+    Args:
+        router (typing.List[typing.Tuple[str,str]], optional): list of tuple contains of source-bq_table pairs
+        prefix (str, optional): Big Query table prefix
+
+    Returns:
+        dict: modified data in dict format (source payload)
+    """
+    route = {elem[0]: "_".join([prefix, elem[1]]) for elem in router}
     return route
 
-def shallow_flatten(data:dict, attr_name:str, is_null:bool=False) -> dict:
+def shallow_flatten(data:dict, attr_name:str, is_null:bool=False, 
+                    sep:str="_") -> dict:
+    """ Flat-nested (only one-level deep, not recursive way) of dictionary data (source payload)
+
+    Args:
+        data (dict): source payload
+        attr_name (str): target attribute name to be flatten_
+        is_null (bool, optional): give null value to all key of related attribute data if true.
+        sep (str, optional): prefix for attribute key-value data.
+
+    Returns:
+        dict: modified data in dict format (source payload)
+    """
     if data[attr_name] is None:
         return data
     else:
         for key, val in data[attr_name].items():
-            data[attr_name+"."+key] = None if is_null else val
+            data[attr_name+sep+key] = None if is_null else val
     return data
 
-def epoch_to_datetime(data:dict) -> dict:
-    time_key_list = ["ts_ms","create", "update"]
-    tz = pytz.timezone('Asia/Jakarta')
-    data = {key: datetime.datetime.fromtimestamp(val/1000, tz).strftime('%Y-%m-%d %H:%M:%S') if 
-        any(sub in key for sub in time_key_list) and (isinstance(val,int) or isinstance(val,float)) 
-        else val for key, val in data.items()}
+
+def extract_key_index(data:dict, table_name_attr:str="source_table" ,
+                      attr_search_list: typing.List[str]=data_change_attr) -> dict:
+    """Extract key index from log data in source payload
+
+    Args:
+        data (dict): source/dataset payload
+        table_name_attr (str, optional): name of table attribute in source payload 
+        attr_search_list (typing.List[str], optional): search list of log data attribute
+
+    Returns:
+        dict: modified data in dict format (source payload)
+    """
+    for ch_attr in attr_search_list:
+        if data[ch_attr] is None:
+            continue
+        else:
+            for key in table_key_index[data["source_table"]]:
+                data[key] = data[ch_attr][key] 
     return data
+
+
+def convert_epoch_to_datetime(data:dict,prefix:str="dt", sep: str="_", 
+                              tz: object = tz,
+                              time_key_list: typing.List[str]=time_key_list) -> dict:
+    """Convert epoch timestamp into datetime with timezone.
+
+    Args:
+        data (dict): source payload
+        prefix (str, optional): prefix for converted attribute data (create new one)
+        sep (str, optional): separator for prefix and related time data key
+        tz (object, optional): timezone for conversion
+        time_key_list (typing.List[str], optional): List of substring of time data attribute in source payload
+
+    Returns:
+        dict: modified data in dict format (source payload)
+    """
+    placeholder = {}
+    for key, val in data.items():
+        if any(sub in key for sub in time_key_list) and (
+            isinstance(val,int) or isinstance(val,float)):
+            placeholder[sep.join([prefix, key])] = datetime.datetime.fromtimestamp(
+                val/1000, tz).strftime('%Y-%m-%d %H:%M:%S') 
+    return {**placeholder, **data}
 
 
 def process_data(data: dict) -> dict:
+    """ Processing source data like breakdown, conversion, and so on.
+
+    Args:
+        data (dict): source payload
+
+    Returns:
+        dict: modified source payload
+    """
     final_data = data["payload"].copy() if "payload" in data.keys() else data.copy()
-    # before_null = False
-    # after_null = False
-    final_data["transaction"] = str(final_data.get("transaction")) if final_data.get("transaction") is not None else None
-    # if final_data["before"] is None and final_data["after"] is not None:
-    #     final_data["before"] = final_data["after"]
-    #     before_null=True
-    # elif final_data["after"] is None and final_data["before"] is not None:
-    #     final_data["after"] = final_data["before"]
-    #     after_null=True
-    # final_data = shallow_flatten(final_data, "after", after_null)
-    # final_data = shallow_flatten(final_data, "before", before_null)
     final_data = shallow_flatten(final_data, "source")
-    xmin = final_data["source.xmin"] 
-    final_data["source.xmin"] = None if not(isinstance(xmin, int)) or not(isinstance(xmin, float)) else float(xmin)
-    # final_data["source.ts_ms"] = datetime.datetime.fromtimestamp(final_data.get("source.ts_ms")/1000, tz).strftime('%Y-%m-%d %H:%M:%S')
-    # final_data["cdc_ts"] = datetime.datetime.fromtimestamp(final_data.get("ts_ms")/1000, tz).strftime('%Y-%m-%d %H:%M:%S')
-    final_data["source.snapshot"] = False if final_data.get("source.snapshot").lower() == "false" else True
-    final_data = epoch_to_datetime(final_data)
+    final_data = extract_key_index(final_data)
+    final_data["before"] = json.dumps(final_data["before"]) if isinstance(
+        final_data["before"],dict) else None 
+    final_data["after"] = json.dumps(final_data["after"]) if isinstance(
+        final_data["after"],dict) else None
+    xmin = final_data["source_xmin"] 
+    final_data["source_xmin"] = None if not(isinstance(xmin, int)) or not(
+        isinstance(xmin, float)) else float(xmin)
+    final_data["source_snapshot"] = False if final_data.get(
+        "source_snapshot").lower() == "false" else True
+    final_data = convert_epoch_to_datetime(final_data)
     del final_data["source"]
+    del final_data["transaction"]
     return final_data
 
 def send_into_bq(data:dict, route: dict=routing_table()):
+    """Send processed source data into specific big query table 
+
+    Args:
+        data (dict): modified source payload
+        route (dict, optional): big query table routing
+    """
     rows_to_insert = [data]
-    if data['source.table'] not in route:
+    if data['source_table'] not in route:
         print("Source is not registered in the table router")
     else:
-        bq_table = route[data['source.table']]
-        print(f"{PROJECT_ID}.{DATASET_ID}.{bq_table}")
-    # errors = client.insert_rows_json(
-    #     f"{PROJECT_ID}.{DATASET_ID}.{route[data['source.table']]}", rows_to_insert
-    # )
-    # if errors == []:
-    #     print("New rows have been added.")
-    # else:
-    #     print("Encountered errors while inserting rows: {}".format(errors))
+        errors = client.insert_rows_json(
+            f"{PROJECT_ID}.{DATASET_ID}.{route[data['source_table']]}", rows_to_insert
+        )
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
 
 
 def main():
-    mydata = process_data(dummy_data_2)
+    """Main execution for data processing and loading 
+    """
     # Add json loads here
+    mydata = process_data(dummy_data_2)
     print(json.dumps(mydata, indent=4, default=str))
     send_into_bq(mydata)
    
